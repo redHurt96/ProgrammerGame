5f325c2345301fb3c4ea2cc0f30cfadd0e6d0b51
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-01-21 16:19:53 +0300

    base mechanics wip

---
+++ Assets/_Game/Scripts/EventsNames.cs
@@ -0,0 +1,7 @@
+namespace AP.ProgrammerGame
+{
+    public static class EventsNames
+    {
+        public const string CODE_WRITTEN = "Code written";
+    }
+}
--- Assets/_Game/Scripts/MoneySpawner.cs
+++ Assets/_Game/Scripts/MoneySpawner.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using RH.HyperCasualUtilities.GlobalEventsSystem;
 using UnityEngine;
 
 namespace AP.ProgrammerGame
@@ -8,13 +6,28 @@ namespace AP.ProgrammerGame
     public class MoneySpawner : MonoBehaviour
     {
         [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private Transform _moneysObjectsParent;
         [SerializeField] private Vector3 _spawnZone;
 
         [SerializeField] private GameObject _moneyPrefab;
 
-        private void Start()
-        {
+        private void Start() => GlobalEvents.Subscribe(EventsNames.CODE_WRITTEN, SpawnMoney);
+        private void OnDestroy() => GlobalEvents.Unsubscribe(EventsNames.CODE_WRITTEN, SpawnMoney);
 
+        private void SpawnMoney()
+        {
+            Vector3 spawnPoint = _spawnPoint.position + 
+                new Vector3(
+                    Random.Range(-_spawnZone.x, _spawnZone.x), 
+                    Random.Range(-_spawnZone.y, _spawnZone.y), 
+                    Random.Range(-_spawnZone.z, _spawnZone.z));
+
+            GameObject money = Instantiate(_moneyPrefab, spawnPoint, Random.rotation, _moneysObjectsParent);
         }
+    }
+
+    public class Wallet
+    {
+        
     }
 }

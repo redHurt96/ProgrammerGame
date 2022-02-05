aa15a3df2ba2be1189268d7a8805f0699e3f0940
redHurt <redhead112358@gmail.com>
2022-02-05 18:51:09 +0300

    switch to 2020.3.23 & install fb analytics

--- ProgrammerGame/Assets/_Game/Scripts/Logic/Systems/FurnitureSpawnSystem.cs
+++ ProgrammerGame/Assets/_Game/Scripts/Logic/Systems/FurnitureSpawnSystem.cs
@@ -6,7 +6,7 @@ namespace AP.ProgrammerGame.Logic
     {
         public static void Instantiate(FurnitureSlot slot)
         {
-            GameObject furniture = Object.Instantiate(slot.Furniture, SceneObjects.Instance.HouseParentObject);
+            GameObject furniture = Object.Instantiate(slot.Furniture, SceneObjects.Instance?.HouseParentObject);
             GlobalEvents.CreateFurniture(slot.Type, furniture);
         }
     }

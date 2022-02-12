6e5e033d4d68dd8d2d6d9506d656b9f9fff874f9
redHurt <redhead112358@gmail.com>
2022-02-12 18:48:07 +0300

    Change code writing

---
+++ ProgrammerGame/Assets/_Game/Scripts/UI/CodeWritingPanelMultiImage.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AP.ProgrammerGame.UI
+{
+    public class CodeWritingPanelMultiImage : MonoBehaviour
+    {
+        [SerializeField] private Image[] _codeImages;
+
+        private void Start()
+        {
+            PrepareImages();
+            GlobalEvents.CodeWritten += Write;
+            GlobalEvents.CodeWrittenComplete += ClearCode;
+        }
+
+        private void OnDestroy()
+        {
+            GlobalEvents.CodeWritten -= Write;
+            GlobalEvents.CodeWrittenComplete -= ClearCode;
+        }
+
+        [ContextMenu(nameof(PrepareImages))]
+        private void PrepareImages()
+        {
+            _codeImages = GetComponentsInChildren<Image>();
+            ClearCode();
+        }
+
+        private void Write()
+        {
+            var progress = GameData.Instance.CodeWritingProgress;
+            var fillValue = progress * _codeImages.Length;
+
+            for (int i = 0; i < _codeImages.Length; i++)
+            {
+                Image image = _codeImages[i];
+                int fillValueInt = (int) fillValue;
+
+                if (i < fillValueInt && image.fillAmount < 1f)
+                {
+                    image.fillAmount = 1;
+                }
+                else if (i == fillValueInt)
+                {
+                    image.fillAmount = fillValue - fillValueInt;
+                    break;
+                }
+            }
+        }
+
+        private void ClearCode()
+        {
+            foreach (var image in _codeImages)
+                image.fillAmount = 0f;
+        }
+    }
+}
---
+++ ProgrammerGame/Assets/_Game/Scripts/UI/CodeWritingPanelSingleImage.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AP.ProgrammerGame.UI
+{
+    public class CodeWritingPanelSingleImage : MonoBehaviour
+    {
+        [SerializeField] private Image _codeImage;
+
+        private void Start() => 
+            GlobalEvents.CodeWritten += Write;
+
+        private void OnDestroy() => 
+            GlobalEvents.CodeWritten -= Write;
+
+        private void Write() => 
+            _codeImage.fillAmount = GameData.Instance.CodeWritingProgress;
+    }
+}

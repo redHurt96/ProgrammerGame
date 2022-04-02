6471e96c933b740f7be2c35499d0e2b690b5cf99
redHurt <redhead112358@gmail.com>
2022-04-02 23:06:26 +0300

    Upgrade and buy projects mechanic

---
+++ ProgrammerGame/Assets/_Game/Scripts/Logic/Systems/UpdateProjectAvailabilitySystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace AP.ProgrammerGame.Logic
+{
+    public class UpdateProjectAvailabilityDataSystem
+    {
+        public UpdateProjectAvailabilityDataSystem()
+        {
+            foreach (ProjectData project in GameData.Instance.Projects) 
+                project.Purchased += UpdateProjectDatas;
+        }
+
+        private void UpdateProjectDatas()
+        {
+            foreach (ProjectData project in GameData.Instance.Projects)
+            {
+                ProjectSettings settings = Settings.Instance.ProjectsSettings
+                    .First(x => x.Name == project.Name);
+                
+                if (settings.BlockProject == null || project.ProjectState != ProjectState.NotAvailable)
+                    continue;
+            }
+        }
+    }
+}

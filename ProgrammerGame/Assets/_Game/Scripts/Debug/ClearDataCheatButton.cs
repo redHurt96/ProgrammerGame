dbf4a662b8b1440391b2db381638d62c60882eee
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-05-14 00:21:41 +0300

    Add protection to cheat reset button

--- ProgrammerGame/Assets/_Game/Scripts/Debug/ClearDataCheatButton.cs
+++ ProgrammerGame/Assets/_Game/Scripts/Debug/ClearDataCheatButton.cs
@@ -1,11 +1,22 @@
 ﻿using RH.Utilities.UI;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace _Game.Debug
 {
     public class ClearDataCheatButton : BaseActionButton
     {
-        protected override void PerformOnClick() => 
-            SceneManager.LoadScene(sceneBuildIndex: 0);
+        [SerializeField] private int _targetClickCount;
+
+        private int _clickCount;
+
+        protected override void PerformOnClick()
+        {
+            if (_targetClickCount == ++_clickCount)
+            {
+                _clickCount = 0;
+                SceneManager.LoadScene(sceneBuildIndex: 1);
+            }
+        }
     }
 }

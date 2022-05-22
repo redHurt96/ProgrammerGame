0e0053ac0a21783154b1dfa2d2ba8f19bf48f6e3
redHurt <62645756+redHurt96@users.noreply.github.com>
2022-05-22 13:47:34 +0300

    wip

---
+++ ProgrammerGame/Assets/_Game/Scripts/UI/Tutorial/MoveUpScroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Game.UI.Tutorial
+{
+    public class MoveUpScroll : MonoBehaviour
+    {
+        [SerializeField] private ScrollRect _scrollRect;
+
+        private void Start() => 
+            _scrollRect.verticalNormalizedPosition = 0;
+    }
+}

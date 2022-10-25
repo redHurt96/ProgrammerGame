fbbd5c3ee78b64b723f792a50a25b0b94c26891b
redHurt <redhead112358@gmail.com>
2022-10-25 21:21:59 +0300

    Add random to character animations

---
+++ ProgrammerGame/Assets/_Game/Scripts/Characters/AnimationRandomizer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Game.Characters
+{
+    public class AnimationRandomizer : MonoBehaviour
+    {
+        private static readonly int _idleCycleOffset = Animator.StringToHash("IdleCycleOffset");
+        
+        [SerializeField] private Animator _animator;
+
+        private void Start() => 
+            _animator.SetFloat(_idleCycleOffset, Random.value);
+    }
+}
